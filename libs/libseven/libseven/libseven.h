8fea9d7cec8314c547696be0d192a4492910f043
Kirn Gill <segin2005@gmail.com>
2010-01-20 02:53:12 +0000

    Made header a public header.

--- libs/libseven/libseven/libseven.h
+++ libs/libseven/libseven/libseven.h
@@ -33,6 +33,22 @@ struct tState {
 	ITaskbarList3 *taskbar;
 };
 
+LIBSEVEN_API void InitializeTaskbar(void);
+LIBSEVEN_API void UpdateProgressBar(int current, int maximum);
+LIBSEVEN_API void AssociateHwnd(HWND win);
+LIBSEVEN_API void SetProgressType(int type);
+LIBSEVEN_API void InitThumbButtons(int buttons);
+LIBSEVEN_API int SetThumbButtonDefaultMask(int index);
+LIBSEVEN_API int SetThumbButtonTooltipA(int index, char *tooltip);
+LIBSEVEN_API int SetThumbButtonTooltipW(int index, wchar_t* tooltip);
+#ifdef UNICODE
+#define SetThumbButtonTooltip SetThumbButtonTooltipW
+#else
+#define SetThumbButtonTooltip SetThumbButtonTooltipA
+#endif
+LIBSEVEN_API int SetThumbButtonMask(int index, int mask);
+LIBSEVEN_API void FreeThumbButtons(void);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif 
