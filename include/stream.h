5ac6b3291df942da308fa654ddb2cacb85946f9c
Kirn Gill <segin2005@gmail.com>
2011-07-12 03:36:37 +0000

    * added new audio classes  * other small edits

---
+++ include/stream.h
@@ -0,0 +1,16 @@
+#ifndef STREAM_H
+#define STREAM_H
+
+
+class Stream
+{
+    public:
+        /** Default constructor */
+        Stream();
+        /** Default destructor */
+        virtual ~Stream();
+    protected:
+    private:
+};
+
+#endif // STREAM_H
