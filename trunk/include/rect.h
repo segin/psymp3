7c65b15c7fcd0b2de0d5c9fa4d8f5d986fb5ef75
segin2005 <segin2005@d8a1aaa4-70d1-11de-baec-69240fdc299a>
2011-07-14 22:38:11 +0000

    Added Rect class for wrapping SDL_Rect struct. Probably a lame wasteful thing but it makes code look pretty.

---
+++ trunk/include/rect.h
@@ -0,0 +1,20 @@
+#ifndef RECT_H
+#define RECT_H
+
+
+class Rect
+{
+    public:
+        Rect();
+        virtual ~Rect();
+        unsigned int width() { return m_width; };
+        unsigned int height() { return m_height; };
+        void width(unsigned int a) { m_width = a; };
+        void height(unsigned int a) { m_height = a; };
+    protected:
+    private:
+        unsigned int m_width;
+        unsigned int m_height;
+};
+
+#endif // RECT_H
---
+++ trunk/src/rect.cpp
@@ -0,0 +1,11 @@
+#include "psymp3.h"
+
+Rect::Rect()
+{
+    //ctor
+}
+
+Rect::~Rect()
+{
+    //dtor
+}
