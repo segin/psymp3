dda798b03e56d1aa1dd3cbf0f9f670092333588f
segin2005 <segin2005@d8a1aaa4-70d1-11de-baec-69240fdc299a>
2011-07-29 09:08:12 +0000

    track class now using TagLib::String to simply handling of mixed string/wstring

--- trunk/src/stream.cpp
+++ trunk/src/stream.cpp
@@ -1,3 +1,24 @@
+/*
+ * stream.cpp - Stream functionality base class
+ * This file is part of PsyMP3.
+ * Copyright © 2011 Kirn Gill <segin2005@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
 #include "psymp3.h"
 
 Stream::Stream()
