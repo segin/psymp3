146428a579a39dd539725ed6a25b29160e0c8190
segin2005 <segin2005@d8a1aaa4-70d1-11de-baec-69240fdc299a>
2011-11-13 09:45:54 +0000

    Adding SQLite3 to the source tree

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ `pkg-config --cflags --libs sdl libmpg123 taglib libvisual-0.4 vorbisfile SDL_gfx` main.cpp -o psymp3 -Iinclude src/*.cpp -lSDL_ttf
+g++ `pkg-config --cflags --libs sdl libmpg123 taglib libvisual-0.4 vorbisfile SDL_gfx` -o psymp3 -Iinclude src/*.cpp -lSDL_ttf
