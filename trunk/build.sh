6b0c77bba74e3a7be43f8215f71222542aee6f60
segin2005 <segin2005@d8a1aaa4-70d1-11de-baec-69240fdc299a>
2011-11-13 10:12:10 +0000

    Updated mingw32 build script, noted Unix build script as deprecated.

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# A hackish way of building it all. Deprecated; DO NOT USE!
 
 g++ `pkg-config --cflags --libs sdl libmpg123 taglib libvisual-0.4 vorbisfile SDL_gfx` -o psymp3 -Iinclude src/*.cpp -lSDL_ttf
--- trunk/mingw32-build.sh
+++ trunk/mingw32-build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-mingw32-g++ `PKG_CONFIG_PATH="/usr/home/segin/mingw32/lib/pkgconfig" pkg-config --cflags --libs sdl libmpg123 taglib libvisual-0.4 vorbisfile SDL_gfx` -o psymp3.exe -Iinclude src/*.cpp -lSDL_ttf -DPSYMP3_DATADIR=\".\" ~/mingw32/lib/*.dll.a
+# mingw32 build method. Requires that you provide --prefix=$HOME/mingw32 to 
+# dependency ./configure passes (that is, to SDL and friends) 
+
+mingw32-g++ `PKG_CONFIG_PATH="${HOME}/mingw32/lib/pkgconfig" pkg-config --cflags --libs sdl libmpg123 taglib libvisual-0.4 vorbisfile SDL_gfx` -o psymp3.exe -Iinclude src/*.cpp -lSDL_ttf -DPSYMP3_DATADIR=\".\" ~/mingw32/lib/*.dll.a
