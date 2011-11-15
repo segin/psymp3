fa4533d45b1a76c248cb2720e4ba341e4234f80b
segin2005 <segin2005@d8a1aaa4-70d1-11de-baec-69240fdc299a>
2011-11-15 16:14:03 +0000

    * Modified win32 build script to compile in sqlite3.c and the resouces  * Reduced size of audio buffer  * Added some code for getting storage directories  * Added some definitions related to Windows 7 Taskbar APIs  * Other fixups, I can't quite remember.

--- trunk/src/system.cpp
+++ trunk/src/system.cpp
@@ -4,3 +4,21 @@ System::System()
 {
     //ctor
 }
+
+TagLib::String System::getHome()
+{
+    #ifdef _WIN32
+        return getenv("USERPROFILE");
+    #else
+        return getenv("HOME");
+    #endif
+}
+
+TagLib::String System::getStoragePath()
+{
+    #ifdef _WIN32
+        return getenv("APPDATA") + "\PsyMP3";
+    #else
+        return getHome() + "/.psymp3";
+    #endif
+}
