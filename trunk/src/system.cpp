ef2af56dd520a33783fcdb6cb553569c7c49893e
segin2005 <segin2005@d8a1aaa4-70d1-11de-baec-69240fdc299a>
2011-11-16 02:46:05 +0000

    Documented the API in the System class.

--- trunk/src/system.cpp
+++ trunk/src/system.cpp
@@ -5,6 +5,21 @@ System::System()
     //ctor
 }
 
+/* Get the current user's home directory. This is determined via
+ * environment variables. On Windows, one could alternatively use
+ * GetUserProfileDirectory(), along with OpenProcessToken() and
+ * GetCurrentProcess(), but that seems a roundabout way of acomplishing
+ * something so trivial when there's an environment variable that
+ * does the same thing
+ * Note: Seems I could also do SHGetFolderPath(), except then I need
+ * to check whether we're on Vista and later, or XP and earlier, and
+ * SHGetKnownFolderPath() is the one to use on Vista - and it doesn't
+ * exist on earlier Windows releases. Which makes using it non-trivial
+ * as one must then LoadLibrary(), etc., to get the procedure entry point
+ * to avoid having a static reference in the symbol table, as that would
+ * break binary compatibility with anything earlier than Vista.
+ */
+
 TagLib::String System::getHome()
 {
     #ifdef _WIN32
@@ -14,6 +29,11 @@ TagLib::String System::getHome()
     #endif
 }
 
+/* This function returns the storage path. This is determined based on
+ * the %AppData% environment variable on Windows, and the $HOME
+ * environment variable on Unix.
+ */
+
 TagLib::String System::getStoragePath()
 {
     #ifdef _WIN32
@@ -21,4 +41,13 @@ TagLib::String System::getStoragePath()
     #else
         return getHome() + "/.psymp3";
     #endif
+}
+
+/* Creates the storage path specified by getStoragePath() if it does
+ * not already exist.
+ */
+
+bool System::createStoragePath()
+{
+
 }
