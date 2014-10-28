e1de1964c9aaa7c51c28f7b07be5480bfe3d327b
Kirn Gill <segin2005@gmail.com>
2014-10-28 23:14:29 +0000

    Added skeleton for Widget class.

---
+++ src/Widget.cpp
@@ -0,0 +1,35 @@
+/*
+ * Widget.cpp - Extensible widget class, extends Surface.
+ * This file is part of PsyMP3.
+ * Copyright © 2014 Kirn Gill <segin2005@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+Widget::Widget()
+{
+    //ctor
+}
+
+Widget::~Widget()
+{
+    //dtor
+}
+
+Widget::Widget(const Widget& other)
+{
+    //copy ctor
+}
