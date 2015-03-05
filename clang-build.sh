d9be6f1ee11fed1e036c39617e7d4a8ab2e85d3d
Kirn Gill <segin2005@gmail.com>
2015-03-05 03:19:12 -0600

    Added clang-build.sh from mat-psymp3 branch

---
+++ clang-build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# A hackish way of building it all. Deprecated; DO NOT USE!
+# Correction: Better than autotools, maybe it should be used...
+
+
+# i needed the following packages for debian
+#   libSDL-dev
+#   libmpg123-dev
+#   libSDL-gfx1.2-dev
+#   libSDL-ttf2.0-dev
+#   libvorbis-dev
+#   libflac++-dev
+#   libtag1-dev
+
+clang -ggdb3 `pkg-config --cflags sdl libmpg123 taglib vorbisfile SDL_gfx` -o psymp3 -DPSYMP3_DATADIR="\"/usr/share/psymp3\"" -Iinclude src/psymp3.final.cpp -lSDL_ttf `pkg-config --libs sdl libmpg123 taglib vorbisfile SDL_gfx` -march=native -lstdc++
+
