6a10e2f121b3e891e2a3c00fb38950aaa55b65a6
Kirn Gill II <segin2005@gmail.com>
2024-03-28 18:05:11 -0500

    Updated copyright notice

--- src/playlist.cpp
+++ src/playlist.cpp
@@ -1,7 +1,7 @@
 /*
  * playlist.cpp - class implementation for playlist class
  * This file is part of PsyMP3.
- * Copyright © 2011-2020 Kirn Gill <segin2005@gmail.com>
+ * Copyright © 2011-2024 Kirn Gill <segin2005@gmail.com>
  *
  * PsyMP3 is free software. You may redistribute and/or modify it under
  * the terms of the ISC License <https://opensource.org/licenses/ISC>
