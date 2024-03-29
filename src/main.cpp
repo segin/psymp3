81d8ed737633ca3bdda2379545945d0e19667204
Kirn Gill II <segin2005@gmail.com>
2024-03-29 02:22:05 -0500

    Take care of Unicode arguments on Windows.

--- src/main.cpp
+++ src/main.cpp
@@ -23,13 +23,57 @@
 
 #include "psymp3.h"
 
+#ifdef _WIN32
+// Convert wide-character string to UTF-8
+std::string WideCharToUTF8(const wchar_t* wideStr) {
+    int wideStrLen = wcslen(wideStr);
+    int utf8StrLen = WideCharToMultiByte(CP_UTF8, 0, wideStr, wideStrLen, NULL, 0, NULL, NULL);
+    if (utf8StrLen == 0) {
+        return ""; // Conversion failed
+    }
+    std::vector<char> buffer(utf8StrLen + 1); // +1 for null terminator
+    WideCharToMultiByte(CP_UTF8, 0, wideStr, wideStrLen, buffer.data(), utf8StrLen, NULL, NULL);
+    return std::string(buffer.data());
+}
+
+// Parse command line arguments on Windows
+std::vector<std::string> ParseCommandLine(int argc, char *argv[]) {
+    std::vector<std::string> args;
+
+    LPWSTR* wideArgv;
+    int wideArgc;
+
+    wideArgv = CommandLineToArgvW(GetCommandLineW(), &wideArgc);
+    if (wideArgv == NULL) {
+        std::cerr << "Failed to parse command line" << std::endl;
+        exit(1);
+    }
+
+    // Convert wide-character argv to UTF-8
+    for (int i = 0; i < wideArgc; ++i) {
+        args.push_back(WideCharToUTF8(wideArgv[i]));
+    }
+
+    LocalFree(wideArgv); // Free memory allocated by CommandLineToArgvW
+
+    return args;
+}
+
+#else
+// Parse command line arguments on Linux
+std::vector<std::string> ParseCommandLine(int argc, char *argv[]) {
+    std::vector<std::string> args;
+    for (int i = 0; i < argc; ++i) {
+        args.push_back(argv[i]);
+    }
+    return args;
+}
+#endif
+
+
 int main(int argc, char *argv[])
 {
     Player PsyMP3;
-    std::vector<std::string> args;
-    for(int i=0; i<argc; i++) {
-        args.push_back(argv[i]);
-    }
-    PsyMP3.Run(args);
+    PsyMP3.Run(ParseCommandLine(argc, argv));
     return 0;
 }
