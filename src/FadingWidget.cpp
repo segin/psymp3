fae66da0cc002680674cd6da1145437c3471fd12
Kirn Gill II <segin2005@gmail.com>
2025-06-28 04:02:12 -0500

    Add FadingWidget implementation for fade-in/out functionality
    Add implementation file for fading widget functionality
    
    Introduces the source file for a widget supporting fade-in and fade-out effects, laying groundwork for UI animations. Enhances user experience by enabling smoother visual transitions.

---
+++ src/FadingWidget.cpp
@@ -0,0 +1,22 @@
+/*
+ * FadingWidget.cpp - Implementation for a widget that can fade in and out.
+ * This file is part of PsyMP3.
+ * Copyright © 2025 Kirn Gill <segin2005@gmail.com>
+ *
+ * PsyMP3 is free software. You may redistribute and/or modify it under
+ * the terms of the ISC License <https://opensource.org/licenses/ISC>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that
+ * the above copyright notice and this permission notice appear in all
+ * copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD to THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA
+ * OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
