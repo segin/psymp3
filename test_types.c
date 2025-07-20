5c496e1a1508a8ff9dec834b39c0c8e9c7b39810
Kirn Gill II <segin2005@gmail.com>
2025-07-20 10:54:10 -0500

    was never to be committed

--- test_types.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <limits.h>
-int main() { 
-    printf("sizeof(off_t) = %zu\n", sizeof(off_t));
-    printf("sizeof(long) = %zu\n", sizeof(long)); 
-    printf("LONG_MAX = %ld\n", LONG_MAX);
-    return 0; 
-}
